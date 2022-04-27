0e7107ec70917c572a8cd211cd0d0d35626f522d
tommady <tommady@users.noreply.github.com>
2022-04-27 09:21:25 +0000

    changing from chrono to time crate

--- src/schema.rs
+++ src/schema.rs
@@ -1,50 +1,52 @@
-use chrono::{DateTime, FixedOffset, NaiveDate, NaiveDateTime};
-use serde::{Deserialize, Serialize};
+use serde::{de, Deserialize, Deserializer, Serialize};
+use time::{Date, PrimitiveDateTime};
 
 use crate::errors::FugleError;
 
 pub type Result<T> = std::result::Result<T, FugleError>;
 
-#[cfg_attr(coverage, no_coverage)]
-pub(crate) fn default_naive_date() -> NaiveDate {
-    NaiveDate::from_num_days_from_ce(1)
+fn de_date<'de, D>(deserializer: D) -> std::result::Result<Date, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    let format =
+        time::format_description::parse("[year]-[month]-[day]").map_err(de::Error::custom)?;
+    Date::parse(&s, &format).map_err(de::Error::custom)
 }
 
-#[cfg_attr(coverage, no_coverage)]
-pub(crate) fn default_date_time() -> DateTime<FixedOffset> {
-    DateTime::<FixedOffset>::from_utc(
-        NaiveDateTime::from_timestamp(0, 0),
-        FixedOffset::east(8 * 3600),
-    )
+pub fn de_primitive_date_time<'de, D>(
+    deserializer: D,
+) -> std::result::Result<PrimitiveDateTime, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    PrimitiveDateTime::parse(&s, &time::format_description::well_known::Rfc3339)
+        .map_err(de::Error::custom)
 }
 
 #[derive(Debug, Deserialize, Serialize)]
-#[serde(rename_all = "camelCase")]
+#[serde(rename_all = "camelCase", default)]
 pub struct Info {
-    #[serde(default = "default_date_time")]
-    pub last_updated_at: DateTime<FixedOffset>,
-    #[serde(default = "default_naive_date")]
-    pub date: NaiveDate,
-    #[serde(default)]
+    #[serde(deserialize_with = "de_primitive_date_time")]
+    pub last_updated_at: PrimitiveDateTime,
+    #[serde(deserialize_with = "de_date")]
+    pub date: Date,
     pub symbol_id: String,
-    #[serde(default)]
     pub country_code: String,
-    #[serde(default)]
     pub time_zone: String,
-    #[serde(default)]
     pub exchange: String,
-    #[serde(default)]
     pub market: String,
-    #[serde(default, rename = "type")]
+    #[serde(rename = "type")]
     pub typ: String,
 }
 
 impl Default for Info {
-    #[cfg_attr(coverage, no_coverage)]
     fn default() -> Info {
         Info {
-            last_updated_at: default_date_time(),
-            date: default_naive_date(),
+            last_updated_at: PrimitiveDateTime::MIN,
+            date: Date::MIN,
             symbol_id: "".to_string(),
             country_code: "".to_string(),
             time_zone: "".to_string(),
@@ -52,5 +54,23 @@ impl Default for Info {
             market: "".to_string(),
             typ: "".to_string(),
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_info_default() {
+        let i = Info::default();
+        // assert_eq!(i.last_updated_at, OffsetDateTime::UNIX_EPOCH);
+        assert_eq!(i.date, Date::MIN);
+        assert_eq!(i.symbol_id, "".to_string());
+        assert_eq!(i.country_code, "".to_string());
+        assert_eq!(i.time_zone, "".to_string());
+        assert_eq!(i.exchange, "".to_string());
+        assert_eq!(i.market, "".to_string());
+        assert_eq!(i.typ, "".to_string());
     }
 }
